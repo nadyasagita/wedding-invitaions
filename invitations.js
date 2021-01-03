3bfbfe5341fb73f2f6106b7092028faa15f54d2e
ebysofyan <eby.sofyan@gmail.com>
2021-01-03 20:23:46 +0800

    Initial commit

---
+++ invitations.js
@@ -0,0 +1,94 @@
+var invitations = [
+    {
+        id: "72fc99e8-b352-11e8-96f8-529269fb1459",
+        name: "Ridatul Aulia"
+    },
+    {
+        id: "72fbe93a-b352-11e8-96f8-529269fb1459",
+        name: "Dr. Akhada Maulana Sp.U Dan Ummi Rini Jati Ayu Kurniati "
+    },
+    {
+        id: "72fc6f7c-b352-11e8-96f8-529269fb1459",
+        name: "M. Amrillah"
+    },
+    {
+        id: "72fc71c0-b352-11e8-96f8-529269fb1459",
+        name: "Juani pratama dan Istri"
+    },
+    {
+        id: "72fc7404-b352-11e8-96f8-529269fb1459",
+        name: "Bunda Dahlia"
+    },
+    {
+        id: "72fc765c-b352-11e8-96f8-529269fb1459",
+        name: "Bunda Rinti"
+    },
+    {
+        id: "72fc7896-b352-11e8-96f8-529269fb1459",
+        name: "Bunda Siti"
+    },
+    {
+        id: "72fc7ae4-b352-11e8-96f8-529269fb1459",
+        name: "Chaca, Dilla, Diyah"
+    },
+    {
+        id: "72fc8048-b352-11e8-96f8-529269fb1459",
+        name: "Dian"
+    },
+    {
+        id: "72fc82b4-b352-11e8-96f8-529269fb1459",
+        name: "Dibah Dan Metyu"
+    },
+    {
+        id: "72fc8502-b352-11e8-96f8-529269fb1459",
+        name: "Dr. Ima"
+    },
+    {
+        id: "72fc873c-b352-11e8-96f8-529269fb1459",
+        name: "Dr. Maythia Prawisitha"
+    },
+    {
+        id: "72fc8976-b352-11e8-96f8-529269fb1459",
+        name: "Drg. Rina Yulia Wulandari"
+    },
+    {
+        id: "72fc8bba-b352-11e8-96f8-529269fb1459",
+        name: "Firza Dan Suami"
+    },
+    {
+        id: "72fc92f4-b352-11e8-96f8-529269fb1459",
+        name: "Gilang"
+    },
+    {
+        id: "72fc957e-b352-11e8-96f8-529269fb1459",
+        name: "Hj. Farid Dan istri"
+    },
+    {
+        id: "72fc97b8-b352-11e8-96f8-529269fb1459",
+        name: "Mba Ambar Dan Suami"
+    },
+    {
+        id: "72fc9c18-b352-11e8-96f8-529269fb1459",
+        name: "Mba Ika"
+    },
+    {
+        id: "72fc9e2a-b352-11e8-96f8-529269fb1459",
+        name: "Mba Nink"
+    },
+    {
+        id: "72fca320-b352-11e8-96f8-529269fb1459",
+        name: "Ikhwahfillah MU Lombok"
+    },
+    {
+        id: "72fca58c-b352-11e8-96f8-529269fb1459",
+        name: "Jihan, Ayuk, Nanda"
+    },
+    {
+        id: "72fca7da-b352-11e8-96f8-529269fb1459",
+        name: "Ukhtyfillah Tahajjud Call Community"
+    },
+    {
+        id: "72fcaa14-b352-11e8-96f8-529269fb1459",
+        name: "Ukhtyfillah Tahajjud Call NTB"
+    },
+]
